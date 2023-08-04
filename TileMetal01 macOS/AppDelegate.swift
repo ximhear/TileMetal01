0057b83f0a19530bc10462658609ca24b1961d84
gzonelee <gzonelee@gmail.com>
2023-08-05 01:15:21 +0900

    Initial Commit

---
+++ TileMetal01 macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  TileMetal01 macOS
+//
+//  Created by gzonelee on 2023/08/05.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
